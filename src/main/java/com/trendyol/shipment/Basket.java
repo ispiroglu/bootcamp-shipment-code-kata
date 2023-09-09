c26a80d8b54be597f7430433e604e620060fde9f
Evren Ispiroglu <evrenn.ispiroglu@gmail.com>
2023-09-09 16:02:43 +0300

    Initial refactor

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,33 @@
 package com.trendyol.shipment;
 
+import java.util.HashMap;
 import java.util.List;
+
+import static com.trendyol.shipment.ShipmentSize.*;
 
 public class Basket {
 
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        var sizeMap = new HashMap<ShipmentSize, Integer>();
+        var maxSize = SMALL;
+        for (var product: products ) {
+            var productSize = product.getSize();
+            var sizeCount = sizeMap.get(productSize);
+            sizeCount = sizeCount == null ? 0 : sizeCount;
+
+            if (sizeCount == 2) {
+                return productSize.nextSize();
+            }
+
+            sizeMap.put(productSize, sizeCount + 1);
+            if (productSize.isLargerThan(maxSize)) {
+                maxSize = productSize;
+            }
+        }
+
+        return maxSize;
     }
 
     public List<Product> getProducts() {
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -5,5 +5,17 @@ public enum ShipmentSize {
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+
+    private static final ShipmentSize[] sizes = values();
+
+    public ShipmentSize nextSize() {
+        return this == X_LARGE ?
+                X_LARGE :
+                sizes[this.ordinal() + 1];
+    }
+
+    public boolean isLargerThan(ShipmentSize size) {
+        return this.ordinal() > size.ordinal();
+    }
 }
